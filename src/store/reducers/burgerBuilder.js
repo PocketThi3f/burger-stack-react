36a4b64a612223034539b8ba5ca178f80c46abd5
m <m@Apples-Mac-2.local>
2019-04-25 22:38:50 -0700

    cleaning up more code

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,8 +1,9 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updatedObject } from '../utility';
 
 const initialState = {
     toppings: null,
-    totalPrice: 2,
+    totalPrice: 1.50,
     error: false
 };
 
@@ -13,39 +14,53 @@ const INGREDIENT_PRICES = {
     bacon: 0.85
 };
 
+// Adding more constants to make code look leaner and cleaner
+const addTopping = (state, action) => {
+    const updatedTopping = { [action.toppingName]: state.toppings[action.toppingName] + 1 };
+    const updatedToppings = updatedObject(state.toppings, updatedTopping);
+    const updatedState = {
+        toppings: updatedToppings,
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.toppingName]
+    }
+    return updatedObject(state, updatedState);
+};
+
+const removeTopping = (state, action) => {
+    const updatedTopp = { [action.toppingName]: state.toppings[action.toppingName] - 1 };
+    const updatedTopps = updatedObject(state.toppings, updatedTopp);
+    const updatedSt = {
+        toppings: updatedTopps,
+        totalPrice: state.totalPrice - INGREDIENT_PRICES[action.toppingName]
+    }
+    return updatedObject(state, updatedSt);
+};
+
+const setToppings = (state, action) => {
+    return updatedObject(state, {
+        ...state,
+            toppings: action.toppings, // Here you would need to set a new object and list as action.toppings.salad (manually changing order of ingredients)
+            totalPrice: 1.50,
+            error: false
+    });
+};
+
+const fetchToppingError = (state, action) => {
+    return updatedObject(state, {error: true});
+};
+
+// Ends here
+
 // Demo of deep cloning states with copying and pasting into a newly created object
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.ADD_TOPPING:
-            return {
-                ...state,
-                toppings: {
-                    ...state.toppings,
-                    [action.toppingName]: state.toppings[action.toppingName] + 1
-                },
-                totalPrice: state.totalPrice + INGREDIENT_PRICES[action.toppingName]
-            }
+            return addTopping(state, action);
         case actionTypes.REMOVE_TOPPING:
-            return {
-                ...state,
-                toppings: {
-                    ...state.toppings,
-                    [action.toppingName]: state.toppings[action.toppingName] - 1
-                },
-                totalPrice: state.totalPrice - INGREDIENT_PRICES[action.toppingName]
-            }
+            return removeTopping(state, action);
         case actionTypes.SET_TOPPINGS:
-            return {
-                ...state,
-                toppings: action.toppings, // Here you would need to set a new object and list as action.toppings.salad (manually changing order of ingredients)
-                totalPrice: 2,
-                error: false
-            }
+            return setToppings(state, action);
         case actionTypes.FETCH_TOPPING_ERROR:
-            return {
-                ...state,
-                error: true
-            }
+            return fetchToppingError(state, action);
         default:
             return state;
     }
--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updatedObject } from '../utility';
 
 const initialState = {
     orders: [],
@@ -6,50 +7,61 @@ const initialState = {
     purchased: false
 };
 
+// Adding constants to clean up code
+const purchaseInit = (state, action) => {
+    return updatedObject(state, {purchased: false});
+};
+
+const purchaseBurgerStart = (state, action) => {
+    return updatedObject(state, {loading: true});
+};
+
+const purchaseSuccess = (state, action) => {
+    const newOrder = updatedObject(action.orderData, {id: action.orderId});
+        return updatedObject(state, {
+            loading: false,
+            purchased: true,
+            orders: state.orders.concat(newOrder)
+        });
+};
+
+const purchaseCancel = (state, action) => {
+    return updatedObject(state, {loading: false});
+};
+
+const fetchOrdersStart = (state, action) => {
+    return updatedObject(state, {loading: true});
+};
+
+const fetchOrdersSuccess = (state, action) => {
+    return updatedObject(state, {
+        orders: action.orders,
+        loading: false
+    });
+};
+
+const fetchOrdersCancel = (state, action) => {
+    return updatedObject(state, {loading: false});
+};
+
+// Ends Here
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.PURCHASE_INIT:
-            return {
-                ...state,
-                purchased: false
-            };
+            return purchaseInit(state, action);
         case actionTypes.PURCHASE_BURGER_START:
-            return {
-                ...state,
-                loading: true
-            };
+            return purchaseBurgerStart(state, action);
         case actionTypes.PURCHASE_SUCCESS:
-            const newOrder = {
-                ...action.orderData,
-                id: action.orderId,
-            };
-            return {
-                ...state,
-                loading: false,
-                purchased: true,
-                orders: state.orders.concat(newOrder)
-            };
+            return purchaseSuccess(state, action);
         case actionTypes.PURCHASE_CANCEL:
-            return {
-                ...state,
-                loading: false
-            };
+            return purchaseCancel(state, action);
         case actionTypes.FETCH_ORDERS_START:
-            return {
-                ...state,
-                loading: true
-            };
+            return fetchOrdersStart(state, action);
         case actionTypes.FETCH_ORDERS_SUCCESS:
-            return {
-                ...state,
-                orders: action.orders,
-                loading: false
-            };
+            return fetchOrdersSuccess(state, action);
         case actionTypes.FETCH_ORDERS_CANCEL:
-            return {
-                ...state,
-                loading: false
-            };
+            return fetchOrdersCancel(state, action);
         default:
             return state;
     }
