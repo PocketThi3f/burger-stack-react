ebab028787911ccff2a1f14627c19634c058890c
m <m@Apples-Mac-2.local>
2019-05-02 21:24:18 -0700

    small added comment for website ref

--- src/index.js
+++ src/index.js
@@ -27,6 +27,7 @@ import * as serviceWorker from './serviceWorker';
 // };
 
 // Enhancers with Advanced Redux (to setup use: https://github.com/zalmoxisus/redux-devtools-extension)
+// Add thunk https://github.com/reduxjs/redux-thunk
 const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
 const rootReducer = combineReducers({
